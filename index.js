3866168245d1585d3a43a4d5da6a8811396bca98
Kris <kristopher_hughes@yahoo.com>
2017-08-28 20:51:19 -0700

    prepare server for heroku

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ var app = express();
 // });
 
 app.use(express.static(__dirname + '/build'));
-
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 });
+
+// const PORT = process.env.PORT || 3000;
+// module.exports = exports = (port, cb) => {
+//   return app.listen(port || PORT,
+//     cb || (() => console.log('Server running on Port: ' + PORT)));
+// };
